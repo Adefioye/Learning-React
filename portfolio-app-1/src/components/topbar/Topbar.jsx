08a1cb5a931a7fc876c7e8372d01a62e6ab95ffe
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-11-12 15:24:38 +0100

    working on 3 lines to the right

--- portfolio-app-1/src/components/topbar/Topbar.jsx
+++ portfolio-app-1/src/components/topbar/Topbar.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import "./topbar.scss";
+import { Person, Email } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar active">
       <div className="wrapper">
         <div className="left">
-          <a href="#intro">Shola</a>
+          <a href="#intro" className="logo">
+            Shola..
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+2349136745490</span>
+          </div>
+          <div className="itemContainer">
+            <Email className="icon" />
+            <span>kevlararamid@gmail.com</span>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line-1"></span>
+            <span className="line-2"></span>
+            <span className="line-3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
