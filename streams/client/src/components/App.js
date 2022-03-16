e92a0528ec00f64e6548fa0d092c62a1ddf0dae1
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-03-16 14:31:18 +0100

    started working on showing a video stream in StreamShow component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -18,9 +18,9 @@ function App() {
           <Header />
           <Switch>
             <Route path="/" exact component={StreamList} />
-            <Route path="/streams/show" exact component={StreamShow} />
             <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit/:id" exact  component={StreamEdit} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
             <Route path="/streams/delete/:id" exact component={StreamDelete} />
           </Switch>
         </div>
